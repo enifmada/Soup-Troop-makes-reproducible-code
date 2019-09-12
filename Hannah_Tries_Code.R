4e710026f5383a0766edb7dc1ba2f933b40be764
Hannah Farrell <hannahfarrell@uchicago.edu>
2019-09-12 17:01:07 -0400

    Added weevil means and variables

--- Hannah_Tries_Code.R
+++ Hannah_Tries_Code.R
@@ -1,5 +1,8 @@
 spiders <- read_csv("cole_arthropod_data_1946.csv")
+weevils <- read_csv("mitchell_weevil_egg_data_1975.csv")
+as.data.frame(weevils)
 as.data.frame(spiders)
+number_weevils <- weevils$k_number_of_eggs
 number_arthropods <- spiders$k_number_of_arthropods
 count_boards_k_spiders <- spiders$C_count_of_boards_with_k_spiders
 count_boards_k_sowbugs <- spiders$C_count_of_boards_with_k_sowbugs
@@ -18,5 +21,8 @@ mean_sowbugs <- total_sowbugs/total_sowbug_boards
 #plot the Poisson distribution of sowbug counts counts
   #add a curve based on the sowbug parameters
 #find the mean 0f the weevil egg counts (in eggs per bean)
+total_weevils <- sum(number_weevils*weevils$C_count_of_beans_with_k_eggs)
+total_weevil_boards <- sum(weevils$C_count_of_beans_with_k_eggs)
+mean_weevils <- total_weevils/total_weevil_boards
 #plot the Poisson distribution of weevil counts
   #add a curve based on the weevil parameters
