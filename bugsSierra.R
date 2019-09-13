f128013dc37d738e226724089eb461b2a1571065
sierra schwabach <schwabach@uchicago.edu>
2019-09-13 11:20:34 -0400

    Tried to do LGP distribution
    I dont know anything

--- bugsSierra.R
+++ bugsSierra.R
@@ -14,6 +14,8 @@ plot_poisson_spider_counts <- function(spider_data) {
                colour='#006400', shape=0, size = 3)
   g_spiders
   
+  g_spiders <- 
+  
   return(g_spiders)
 }
 
@@ -29,7 +31,14 @@ avg_spiders_per_obs <- total_number_spiders/total_board_observations
 #making theoretical propbabilities 
 p_theoretical_spiders <- dpois(cole_arthropod_data_1946$k_number_of_arthropods, avg_spiders_per_obs)
 
-#Theoretical number of times you would observe k spiders
-cole_arthropod_data_1946$count_spiders_theoretical <- total_board_observations*p_theoretical_spiders
+#Poisson Theoretical number of times you would observe k spiders in new column
+cole_arthropod_data_1946$count_spiders_p_theoretical <- total_board_observations*p_theoretical_spiders
 
-plot_poisson_spider_counts(cole_arthropod_data_1946)
+#making theoretical propbabilities with LGP
+L_theoretical_spiders <- dLGP(cole_arthropod_data_1946$k_number_of_arthropods, avg_spiders_per_obs)
+
+#LGP Theoretical number of times you would observe k spiders in new column
+cole_arthropod_data_1946$count_spiders_L_theoretical <- total_board_observations*L_theoretical_spiders
+#This doesnt work, getting error message:
+#Error in LGP.get.nc(theta = theta, lambda = lambda):
+#argument "lambda" is missing, with no default
