723ce28178da6d4f2ca21274d58752a978c6fb32
sierra schwabach <schwabach@uchicago.edu>
2019-09-13 13:32:33 -0400

    Complete plot for spiders
    someone should check me or maybe add a legend for the different distribution lines

--- bugsSierra.R
+++ bugsSierra.R
@@ -1,5 +1,3 @@
-#Plot # of spiders on X axis
-
 
 
 plot_poisson_spider_counts <- function(spider_data) {
@@ -8,16 +6,23 @@ plot_poisson_spider_counts <- function(spider_data) {
     geom_point(size = 3) + xlab("# of spiders") + ylab("count")
   g_spiders
   
-  g_spiders <- g_spiders + geom_line(data=spider_data, aes(x=k_number_of_arthropods, y=count_spiders_theoretical),  
-                               linetype='dashed', colour='#006400') +  
-    geom_point(data=spider_data, aes(x=k_number_of_arthropods, y=count_spiders_theoretical),  
-               colour='#006400', shape=0, size = 3)
+  g_spiders <- g_spiders + geom_line(data=spider_data, aes(x=k_number_of_arthropods, y=count_spiders_p_theoretical, colour='#006400'),  
+                               linetype='dotted', colour='#006400') +  
+    geom_point(data=spider_data, aes(x=k_number_of_arthropods, y=count_spiders_p_theoretical),  
+               colour='#006400', shape=0, size = 5)
   g_spiders
   
-  g_spiders <- 
+  g_spiders <- g_spiders + geom_line(data=spider_data, aes(x=k_number_of_arthropods, y=count_spiders_L_theoretical),  
+                                     linetype='dashed', colour='orchid') +  
+    geom_point(data=spider_data, aes(x=k_number_of_arthropods, y=count_spiders_p_theoretical),  
+               colour='orchid', shape=0, size = 3)
+  g_spiders
   
   return(g_spiders)
 }
+plot_poisson_spider_counts(cole_arthropod_data_1946)
+
+
 
 #Calculated total number of spiders
 total_number_spiders <- sum(cole_arthropod_data_1946$k_number_of_arthropods*cole_arthropod_data_1946$C_count_of_boards_with_k_spiders)
@@ -28,17 +33,16 @@ total_board_observations <- sum(cole_arthropod_data_1946$C_count_of_boards_with_
 #Calculate average (lambda)
 avg_spiders_per_obs <- total_number_spiders/total_board_observations
 
-#making theoretical propbabilities 
+#making theoretical propbabilities (dpois(x, lambda, log = FALSE))
 p_theoretical_spiders <- dpois(cole_arthropod_data_1946$k_number_of_arthropods, avg_spiders_per_obs)
 
-#Poisson Theoretical number of times you would observe k spiders in new column
+#Poisson Theoretical number of times you would observe k spiders (in new column)
 cole_arthropod_data_1946$count_spiders_p_theoretical <- total_board_observations*p_theoretical_spiders
 
-#making theoretical propbabilities with LGP
-L_theoretical_spiders <- dLGP(cole_arthropod_data_1946$k_number_of_arthropods, avg_spiders_per_obs)
+#making theoretical propbabilities with LGP (dLGP(x,theta,lambda,nc=NULL,log=FALSE))
+L_theoretical_spiders <- dLGP(cole_arthropod_data_1946$k_number_of_arthropods,avg_spiders_per_obs,0)
 
-#LGP Theoretical number of times you would observe k spiders in new column
+#LGP Theoretical number of times you would observe k spiders (in new column)
 cole_arthropod_data_1946$count_spiders_L_theoretical <- total_board_observations*L_theoretical_spiders
-#This doesnt work, getting error message:
-#Error in LGP.get.nc(theta = theta, lambda = lambda):
-#argument "lambda" is missing, with no default
+#Theta (or lambda2 in the workbook) is 0, as stated in the workbook. When lambda2 is = 0, 
+#then we get the same as the Poisson distribution
