db0be6480214c6cc2655801005b3290dc0b2ef9e
Emma <stewarte@uchicago.edu>
2019-09-14 16:41:42 -0400

    update q1 and 2

--- ES_reproducibility.R
+++ ES_reproducibility.R
@@ -5,7 +5,7 @@ library(stats)
 library(tidyverse)
 
 #loading arthropod dataset
-arthropod_data <- read_csv("../reproducibility/data/cole_arthropod_data_1946.csv")
+arthropod_data <- read_csv("../data/cole_arthropod_data_1946.csv")
 
 #rename columns, check headers, if its a data frame and the tibble dimensions
 names(arthropod_data) <- c("arthropod count", "spiders on boards", "sowbugs on boards")
@@ -20,27 +20,32 @@ spider_lambda2 <- 0
 sowbug_lambda1 <- sum(arthropod_data$`arthropod count`*arthropod_data$`sowbugs on boards`)/sum(arthropod_data$`sowbugs on boards`)
 sowbug_lambda2 <- 0.53214
 
-#make a plot to look at spider count vs board counts (data points)
-library(ggplot2)
-spider_data_plot <- ggplot(arthropod_data, aes(x = `arthropod count`, y = `spiders on boards`)) + geom_point()
-spider_data_plot
+#calculating theoretical values for Poisson distribution using calculated (lambda1s) for the spider and sowbug data
+theoretical_spider_Poiss <- dpois(arthropod_data$`arthropod count`, spider_lambda1, log = FALSE)
+theoretical_sowbug_Poiss <- dpois(arthropod_data$`arthropod count`, sowbug_lambda1, log = FALSE) 
 
-#adding the Poisson distribution curve to the plot for spider data
-addLGPCurveToPlot <- function(plot, data_in_plot, lambda1, lambda2){
+#total arthropod counts
+total_spiders <- sum(arthropod_data$`spiders on boards`)
+total_sowbugs <- sum(arthropod_data$`sowbugs on boards`)
+
+#theoretical Poisson counts for spiders and sowbugs to use in the final plot
+spider_Poiss_counts <- total_spiders*theoretical_spider_Poiss
+sowbug_Poiss_counts <- total_sowbugs*theoretical_sowbug_Poiss
+
+#make Poisson counts into a data fram and add to arthropod data table
+spider_Poiss_counts_df <- as.data.frame(spider_Poiss_counts)
+sowbug_Poiss_counts_df <- as.data.frame(sowbug_Poiss_counts)
+
+arthropod_data_final <- cbind(arthropod_data, spider_Poiss_counts_df, sowbug_Poiss_counts_df)
+
+# graph the final plots for spiders and sowbugs
+spider_data_plot <- ggplot(arthropod_data_final, aes(x = `arthropod count`, y = `spiders on boards`)) + geom_point()
+spider_Poiss_plot <- ggplot(arthropod_data_final, aes(x = `arthropod count`, y = `spider_Poiss_counts`)) + geom_line()
+
+
+sowbug_data_plot <- ggplot(arthropod_data_final, aes(x = `arthropod count`, y = `sowbugs on boards`)) + geom_point()
+sowbug_Poiss_plot <- ggplot(arthropod_data_final, aes(x = `arthropod count`, y = `sowbug_Poiss_counts`)) + geom_line()
+
+
   
-  LGPCurve <- dLGP(nrow(data_in_plot), lambda1, lambda2)
-  # dataInPlot is the data file used to draw whichever plot you pass to this function
-  # dLGP() returns numeric vectors of probabilities
-  # use this vector to draw LGP curve 
-  
-  show(plot + geom_line(LGPCurve))
-  # overlay Lagrangian Poisson Distribution on plot passed as argument 
-  
-}
 
-addLGPCurveToPlot(spider_data_plot, arthropod_data, spider_lambda1, spider_lambda2)
-
-#plotting the Poisson distribution with the same mean as the sowbug counts, along with the data 
-
-#loading weevil dataset
-weevil_data = read_csv("mitchell_weevil_egg_data_1975.csv")
